3c13dad3afec0548d484379656fe4ab45b065856
crusadersalad <crusadersalad@gmail.com>
2024-02-05 16:27:29 -0500

    choosing just favorite images and messing with css

--- script/welcomeMessage.js
+++ script/welcomeMessage.js
@@ -1,12 +1,23 @@
-// Get the current hour
-const currentHour = new Date().getHours();
+// Get the current date
+const currentDate = new Date();
+const currentHour = currentDate.getHours();
 
 // Get the welcome message element
 const welcomeMessageElement = document.getElementById('welcome-message');
 
-// Define the messages for different times of the day
+// Define the messages for different dates
 let welcomeMessage = '';
-if (currentHour < 2) {
+
+// Check if it's a special date and set the message accordingly
+if (currentDate.getMonth() === 11 && currentDate.getDate() === 25) {
+    welcomeMessage = 'Merry Christmas!';
+} else if (currentDate.getMonth() === 6 && currentDate.getDate() === 4) {
+    welcomeMessage = 'God bless the USA!';
+} else if (currentDate.getMonth() === 3 && currentDate.getDate() === 17) {
+    welcomeMessage = "Happy St. Patrick's Day!"; 
+} else if (currentDate.getMonth() === 3 && currentDate.getDate() === 17) {
+    welcomeMessage = 'Happy Birthday, Hank!';
+} else if (currentHour < 2) {
     welcomeMessage = 'Go to bed, Hank!'; // Between midnight and 2 AM
 } else if (currentHour < 6) {
     welcomeMessage = 'Go to bed, Hank!'; // Between 2 AM and 4:30 AM
@@ -20,4 +31,5 @@ if (currentHour < 2) {
 
 // Set the welcome message
 welcomeMessageElement.textContent = welcomeMessage;
+
 
